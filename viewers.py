372b5d38928b0c2494e0bc8664f17e7b9304a234
B.C.N.Rao <bcnrao2019@gmail.com>
2021-09-11 19:25:52 +0530

    commit viewers.py

--- viewers.py
+++ viewers.py
@@ -16,18 +16,59 @@ my_img5 = ImageTk.PhotoImage(Image.open("/home/bcnrao/Downloads/dmd-doc-icon.png
 image_list = [my_img1,my_img2,my_img3,my_img4,my_img5]
 
 my_label = Label(image=my_img1)
-my_label.grid()
+my_label.grid(row=0, column=0, columnspan=3)
+
+def forward(image_number):
+    # if image_number > 5:
+    #     image_number = image_number-1
+    global my_label
+    global button_for
+    global button_back  
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+
+
+    button_for = Button(root, text=">>", command=lambda:forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number==5:
+        button_for = Button(root, text=">>", state=DISABLED)
+
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1,column=0)
+    button_for.grid(row=1, column=2)
+
+
+
+def back(image_number):
+    global my_label
+    global button_for
+    global button_back
+
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_for = Button(root, text=">>", command=lambda:forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number==1:
+        button_back = Button(root, text="<<", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1,column=0)
+    button_for.grid(row=1, column=2)
+
+button_back = Button(root, text="<<", command=back, state=DISABLED)
+button_exit = Button(root, text='Exit', command=root.quit)
+button_for = Button(root, text=">>", command=lambda:forward(2))
+
+button_back.grid(row=1,column=0)
+button_exit.grid(row=1, column=1)
+button_for.grid(row=1, column=2)
 
 
 # /home/bcnrao/Downloads
-
-
-
-
-
-
-button_quit = Button(root, text="Quit", command=root.quit)
-
-button_quit.pack()
 root.mainloop()
 
